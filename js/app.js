43c9063666a531453425dbd95295e6befb79a040
NickRumenov <nikolayrumenovsf@gmail.com>
2016-05-22 23:18:40 +0300

    Add many and different things.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,54 @@
 var app = app || {};
 
-var deckBackground = document.getElementById('card5');
-deckBackground.style.backgroundPositionY = '100%';
+var madeFirstDistribution = false;
 
-(function(){
-    for (var i = 0; i < 5; i++) {
+var init = function () {
+
+    if (!madeFirstDistribution) {
+        app.deck = app.deck.slice(5, 52);
+        n = 0;
+    } else {
+        app.deck = app.deck.slice(2, 52);
+        document.getElementById('card3').remove();
+        document.getElementById('card4').remove();
+        n = 3;
+    }
+
+    var cards = [];
+
+    for (i = n; i < 5; i++) {
+        (function (id) {
+            setTimeout(function () {
+                var currentCard = cards[id];
+                currentCard.style.top = cards[id].top + 'px';
+                currentCard.style.left = cards[id].left + 'px';
+                currentCard.style.WebkitTransform = "rotate(360deg)";
+                currentCard.style.MozTransform = "rotate(360deg)";
+                currentCard.style.OTransform = "rotate(360deg)";
+                currentCard.style.msTransform = "rotate(360deg)";
+            }, i * 100);
+        })(i);
+    }
+
+    for (var i = 0; i < 6; i++) {
+
+        var card = document.createElement("div");
+        card.className = 'card';
+        card.id = 'card' + i;
+        card.top = 50 + 300 * Math.floor(i / 3);
+        card.left = 200 + 100 * (i % 3);
+        document.getElementById('table').appendChild(card);
+        cards.push(card);
+    }
+
+    for (var i = n; i < 5; i++) {
         var currentCard = document.getElementById('card' + i);
         currentCard.style.backgroundPositionX = app.deck[i].x + '%';
         currentCard.style.backgroundPositionY = app.deck[i].y + '%';
     }
-}());
+    
+    madeFirstDistribution = true;
+}
 
-app.deck = app.deck.slice(5, 52);
+init();
+
---
+++ js/helpers.js
@@ -0,0 +1,23 @@
+﻿var app = app || {};
+
+document.getElementById("card3").onclick = function () {
+    fadeOut(document.getElementById('card3'));
+};
+
+document.getElementById("card4").onclick = function () {
+    fadeOut(document.getElementById('card4'));
+};
+
+
+function fadeOut(element) {
+    var op = 1;  // initial opacity
+    var timer = setInterval(function () {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.display = 'none';
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 50);
+}
