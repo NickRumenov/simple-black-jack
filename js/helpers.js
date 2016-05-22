b4b15ee2d8008661abca4f43c0ff183172312c62
NickRumenov <nikolayrumenovsf@gmail.com>
2016-05-23 02:15:01 +0300

    Fixing bugs, add dissapear effect of player cards

--- js/helpers.js
+++ js/helpers.js
@@ -1,23 +1,25 @@
 ﻿var app = app || {};
 
-document.getElementById("card3").onclick = function () {
-    fadeOut(document.getElementById('card3'));
-};
+//TODO   To fix DRY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-document.getElementById("card4").onclick = function () {
-    fadeOut(document.getElementById('card4'));
-};
+var card3 = document.getElementById("card3");
+var card4 = document.getElementById("card4");
+
+var card3Back = document.createElement("div");
+var card4Back = document.createElement("div");
+
+document.getElementById("card3").addEventListener("click", function () {
+    document.getElementById("card3").className = 'animated bounceOutLeft';
+    setTimeout(function () {
+        document.getElementById("card3").remove();
+    }, 1000);
+});
+
+document.getElementById("card4").addEventListener("click", function () {
+    document.getElementById("card4").className = 'animated bounceOutLeft';
+    setTimeout(function () {
+        document.getElementById("card4").remove();
+    }, 1000);
+});
 
 
-function fadeOut(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1) {
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 50);
-}
